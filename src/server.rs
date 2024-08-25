72fad2ff59a13babce8a9fa7719cfbd5a09f467e
pokyux <pokyux@mail.ustc.edu.cn>
2024-08-25 19:41:58 +0800

    impl: processor process ack

--- src/server.rs
+++ src/server.rs
@@ -1,17 +1,22 @@
-use std::{collections::HashMap, net::{SocketAddr, UdpSocket}, thread};
+use core::panic;
+use std::{collections::HashMap, net::{SocketAddr, UdpSocket}, sync::{mpsc::{Receiver, Sender}, Arc, Mutex}, thread};
 
-use crate::{data::DataMap, MAX_TRANSMIT_SIZE};
+use crate::{data::DataMap, package::Package, MAX_TRANSMIT_SIZE};
 
 pub struct ClientData {
 	pub sequence: u64,
-	// promise channel
+	pub promise_buf: HashMap<i32, Sender<Package>>,
 	pub data: DataMap,
 }
 
 impl ClientData {
 	#[allow(unused)]
 	pub fn new() -> Self {
-		ClientData { sequence: 0, data: DataMap::new() }
+		ClientData {
+			sequence: 0,
+			promise_buf: HashMap::new(),
+			data: DataMap::new(),
+		}
 	}
 }
 
@@ -34,19 +39,36 @@ impl Server {
 	}
 
 	#[allow(unused)]
-	pub fn start(&'static self) -> ! {
+	pub fn start(&mut self) -> ! {
 		let mut buf = [0 as u8; MAX_TRANSMIT_SIZE];
 		loop {
 			let (recv_size, recv_addr) = self.socket.recv_from(&mut buf).unwrap();
 			if recv_size == 0 { continue; }
 
-			let data = Vec::from(&buf[0..recv_size]);
-			thread::spawn(move || self.process(data, recv_addr));
-		}
-	}
+			let recv_pkg = match Package::from(Vec::from(&buf[0..recv_size])) {
+				Some(pkg) => pkg,
+				None => continue,
+			};
 
-	#[allow(unused)]
-	pub fn process(&'static self, mut data: Vec<u8>, recv_addr: SocketAddr) {
-		todo!()
+			if recv_pkg.identifier == "prt-ack" {
+				// 处理 ack 数据包
+				// 先取出对应 ClientData
+				// 然后从 ClientData.promise_buf 中移除 sequence 对应的 Receiver
+				// 通过 Receiver 发送 pkg，然后在 Drop 时自动回收相关资源
+				match match self.client_data.get_mut(&recv_addr) {
+					Some(s) => s,
+					None => continue,
+				}.promise_buf.remove(&recv_pkg.sequence) {
+					Some(v) => v,
+					None => continue,
+				}.send(recv_pkg);
+				// 处理 ack 的逻辑会更改所有客户端数据，
+				// 所以为了并发安全，ack 的初步处理必须在 Server 主线程中进行
+				// 此处理 ack 的逻辑并不会过度占用计算资源
+				// 因为耗时的数据处理操作在 Channel 另一端的线程中进行
+			}
+
+			todo!()
+		}
 	}
 }
